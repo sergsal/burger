b510bd7e914c80cecce18124ead88f2beb028fe7
sergsal <sergio.saldarriaga88@gmail.com>
2016-09-30 17:55:31 -0400

    fixes spelling error in code

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql');
 var connection;
 
-if (prcess.env.JAWSDB_URL) {
+if (process.env.JAWSDB_URL) {
 	connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else connection = mysql.createConnection({
   host     : 'localhost',
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -15,7 +15,7 @@ router.get('/burgers', function (req, res) {
 });
 
 router.post('/burgers/create', function (req, res) {
-	burger.create(['burger_name', 'devoured'], [req.body.burger_name, false], function () {
+	burger.create(['burger_name', 'devoured'], [req.body.burger_name, req.body.devoured], function () {
 		res.redirect('/burgers');
 	});
 });
