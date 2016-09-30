27dbde407d07e66a76da86e271d511cf25318b5b
sergsal <sergio.saldarriaga88@gmail.com>
2016-09-30 17:36:20 -0400

    updates settings for online database

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,9 @@
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-  port: 3306,
+var connection;
+
+if (prcess.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
   password : 'localhost',
