85c22b78df77d921e2925d1f1ad002c385036fb8
sergsal <sergio.saldarriaga88@gmail.com>
2016-09-30 17:47:10 -0400

    fixes port issue with server

--- server.js
+++ server.js
@@ -21,5 +21,6 @@ app.set('view engine', 'handlebars');
 var routes = require('./controllers/burgers_controller.js');
 app.use('/', routes);
 
-var port = 3000;
-app.listen(port);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
